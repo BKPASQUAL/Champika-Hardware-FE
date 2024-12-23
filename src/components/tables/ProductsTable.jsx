ff85a65deeddbd4f81e775b294bc1b11146b7aaa
Bawantha <bawanthapasqual@gmail.com>
2024-12-23 16:50:48 +0530

    map the data product table

--- src/components/tables/ProductsTable.jsx
+++ src/components/tables/ProductsTable.jsx
@@ -1,68 +1,65 @@
 import React from "react";
-import { Table, Button } from "rsuite";
-import mockUsers from "../../assets/mocks/ProductMocks";
+import { Table } from "rsuite";
+import { useGetAllProductsQuery } from "../../store/api/productApi";
 
 const { Column, HeaderCell, Cell } = Table;
-const data = mockUsers(20);
 
 function ProductsTable() {
+  const { data: productData, isLoading, isError } = useGetAllProductsQuery();
+  console.log("productData", productData?.payload);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error loading products.</div>;
+
+  const handleEditItem = (rowData) => {
+    console.log("Edit item:", rowData);
+  };
+
+  const handleDeleteOpen = (id) => {
+    console.log("Delete item with ID:", id);
+  };
+
   return (
     <div>
       <Table
         height={650}
-        data={data}
+        data={productData?.payload || []}
         onRowClick={(rowData) => {
           console.log(rowData);
         }}
         style={{ width: "100%" }}
       >
-        <Column flexGrow={2} align="center">
-          <HeaderCell className="bg-gray-200 text-gray-700">Code</HeaderCell>
+        <Column flexGrow={1} align="center">
+          <HeaderCell>Code</HeaderCell>
           <Cell dataKey="id" />
         </Column>
 
         <Column flexGrow={3}>
-          <HeaderCell className="bg-gray-200 text-gray-700">
-            Product Name
-          </HeaderCell>
-          <Cell dataKey="firstName" />
+          <HeaderCell>Product Name</HeaderCell>
+          <Cell dataKey="productName" />
         </Column>
 
         <Column flexGrow={2}>
-          <HeaderCell className="bg-gray-200 text-gray-700">MRP</HeaderCell>
-          <Cell dataKey="lastName" />
+          <HeaderCell>MRP</HeaderCell>
+          <Cell dataKey="price" />
         </Column>
 
-        <Column flexGrow={2}>
-          <HeaderCell className="bg-gray-200 text-gray-700">10%</HeaderCell>
-          <Cell dataKey="gender" />
-        </Column>
+        {Array.from({ length: 5 }, (_, index) => (
+          <Column flexGrow={2} key={index}>
+            <HeaderCell>{`${10 + index * 5}%`}</HeaderCell>
+            <Cell>
+              {(rowData) => (
+                rowData.discountedPrices?.[index]?.price?.toFixed(2) || "-"
+              )}
+            </Cell>
+          </Column>
+        ))}
 
         <Column flexGrow={2}>
-          <HeaderCell className="bg-gray-200 text-gray-700">15%</HeaderCell>
-          <Cell dataKey="gender" />
-        </Column>
-
-        <Column flexGrow={2}>
-          <HeaderCell className="bg-gray-200 text-gray-700">20%</HeaderCell>
-          <Cell dataKey="age" />
-        </Column>
-
-        <Column flexGrow={2}>
-          <HeaderCell className="bg-gray-200 text-gray-700">25%</HeaderCell>
-          <Cell dataKey="postcode" />
-        </Column>
-
-        <Column flexGrow={2}>
-          <HeaderCell className="bg-gray-200 text-gray-700">30%</HeaderCell>
-          <Cell dataKey="email" />
-        </Column>
-
-        <Column flexGrow={2}>
-          <HeaderCell className="bg-gray-200 text-gray-700">
-            SELLING PRICE
-          </HeaderCell>
-          <Cell dataKey="email" />
+          <HeaderCell>SELLING PRICE</HeaderCell>
+          <Cell>
+            {(rowData) => rowData.price.toFixed(2)}
+          </Cell>
         </Column>
 
         <Column flexGrow={1}>
--- src/store/Store.js
+++ src/store/Store.js
@@ -1,12 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import api from "./api/api";
 import { supliersApi } from "./api/supplier";
+import { productApi } from "./api/productApi";
 
 export const store = configureStore({
   reducer: {
     [api.reducerPath]: api.reducer,
-    [supliersApi.reducerPath]: api.reducer,
+    [supliersApi.reducerPath]: supliersApi.reducer,
+    [productApi.reducerPath]: productApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(api.middleware, supliersApi.middleware),
+    getDefaultMiddleware().concat(
+      api.middleware,
+      supliersApi.middleware,
+      productApi.middleware
+    ),
 });
